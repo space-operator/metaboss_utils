f0b9e544e79526e04110c08e44655838dd779a29
MGlolenstine <mglolenstine@gmail.com>
2022-10-02 08:56:28 +0200

    Changed set_update_authority to return Signature.

--- src/commands/update/update_authority.rs
+++ src/commands/update/update_authority.rs
@@ -1,74 +1,21 @@
-use super::common::*;
+use solana_sdk::signature::Signature;
 
-pub struct SetUpdateAuthorityAllArgs {
-    pub client: RpcClient,
-    pub keypair: Option<String>,
-    pub payer: Option<String>,
-    pub mint_list: Option<String>,
-    pub cache_file: Option<String>,
-    pub new_authority: String,
-    pub batch_size: usize,
-    pub retries: u8,
-}
+use super::common::*;
 
 pub struct SetUpdateAuthorityArgs<'a> {
     pub client: &'a RpcClient,
     pub keypair: Arc<Keypair>,
     pub payer: Arc<Keypair>,
-    pub mint_account: String,
-    pub new_authority: String,
+    pub mint_account: Pubkey,
+    pub new_authority: Pubkey,
 }
 
-pub async fn set_update_authority_one(
-    client: &RpcClient,
-    keypair_path: Option<String>,
-    mint_account: &str,
-    new_update_authority: &str,
-    keypair_payer_path: Option<String>,
-) -> AnyResult<()> {
-    let solana_opts = parse_solana_config();
-    let keypair = parse_keypair(keypair_path.clone(), solana_opts);
-
-    let solana_opts = parse_solana_config();
-    let keypair_payer = parse_keypair(keypair_payer_path.or(keypair_path), solana_opts);
-
-    let mint_pubkey = Pubkey::from_str(mint_account)?;
-    let update_authority = keypair.pubkey();
-    let new_update_authority = Pubkey::from_str(new_update_authority)?;
-
-    let metadata_account = get_metadata_pda(mint_pubkey);
-
-    let ix = update_metadata_accounts_v2(
-        TOKEN_METADATA_PROGRAM_ID,
-        metadata_account,
-        update_authority,
-        Some(new_update_authority),
-        None,
-        None,
-        None,
-    );
-    let recent_blockhash = client.get_latest_blockhash().await?;
-    let tx = Transaction::new_signed_with_payer(
-        &[ix],
-        Some(&keypair_payer.pubkey()),
-        &[&keypair, &keypair_payer],
-        recent_blockhash,
-    );
-
-    let sig = client.send_and_confirm_transaction(&tx).await?;
-    info!("Tx sig: {:?}", sig);
-    println!("Tx sig: {:?}", sig);
-
-    Ok(())
-}
-
-pub async fn set_update_authority<'a>(args: &SetUpdateAuthorityArgs<'a>) -> Result<(), ActionError> {
-    let mint_pubkey = Pubkey::from_str(&args.mint_account).expect("Invalid mint account");
+pub async fn set_update_authority<'a>(args: &SetUpdateAuthorityArgs<'a>) -> Result<Signature, ActionError> {
+    let mint_pubkey = &args.mint_account;
     let update_authority = args.keypair.pubkey();
-    let new_update_authority =
-        Pubkey::from_str(&args.new_authority).expect("Invalid new update authority");
+    let new_update_authority = args.new_authority;
 
-    let metadata_account = get_metadata_pda(mint_pubkey);
+    let metadata_account = get_metadata_pda(mint_pubkey.clone());
 
     let ix = update_metadata_accounts_v2(
         TOKEN_METADATA_PROGRAM_ID,
@@ -94,52 +41,6 @@ pub async fn set_update_authority<'a>(args: &SetUpdateAuthorityArgs<'a>) -> Resu
         .client
         .send_and_confirm_transaction(&tx).await
         .map_err(|e| ActionError::ActionFailed(args.mint_account.to_string(), e.to_string()))?;
-    info!("Tx sig: {:?}", sig);
-
-    Ok(())
-}
-
-pub struct SetUpdateAuthorityAll {}
-
-#[async_trait]
-impl Action for SetUpdateAuthorityAll {
-    fn name() -> &'static str {
-        "set-update-authority-all"
-    }
-
-    async fn action(args: RunActionArgs) -> Result<(), ActionError> {
-        // Set Update Authority can have an optional payer.
-        set_update_authority(&SetUpdateAuthorityArgs {
-            client: &args.client,
-            keypair: args.keypair.clone(),
-            payer: args.payer.clone(),
-            mint_account: args.mint_account,
-            new_authority: args.new_value,
-        })
-        .await
-    }
-}
-
-pub async fn set_update_authority_all(args: SetUpdateAuthorityAllArgs) -> AnyResult<()> {
-    let solana_opts = parse_solana_config();
-    let keypair = parse_keypair(args.keypair, solana_opts);
-
-    let solana_opts = parse_solana_config();
-    let payer = args
-        .payer
-        .map(|path| parse_keypair(Some(path), solana_opts));
-
-    let args = BatchActionArgs {
-        client: args.client,
-        keypair,
-        payer,
-        mint_list: args.mint_list,
-        cache_file: args.cache_file,
-        new_value: args.new_authority,
-        batch_size: args.batch_size,
-        retries: args.retries,
-    };
-    SetUpdateAuthorityAll::run(args).await?;
-
-    Ok(())
+        
+    Ok(sig)
 }
