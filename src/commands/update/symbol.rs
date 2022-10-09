8ecb915a95380b96d19769fcf08c34ba02d02793
MGlolenstine <mglolenstine@gmail.com>
2022-10-09 14:15:23 +0200

    Updated update_symbols method.

--- src/commands/update/symbol.rs
+++ src/commands/update/symbol.rs
@@ -1,41 +1,31 @@
+use solana_sdk::signature::Signature;
+
 use super::{common::*, update_data};
 
-pub struct UpdateSymbolAllArgs {
-    pub client: RpcClient,
-    pub keypair: Option<String>,
-    pub mint_list: Option<String>,
-    pub cache_file: Option<String>,
-    pub new_symbol: String,
-    pub batch_size: usize,
-    pub retries: u8,
-}
-
-pub struct UpdateSymbolArgs {
-    pub client: Arc<RpcClient>,
-    pub keypair: Arc<Keypair>,
-    pub mint_account: Pubkey,
-    pub new_symbol: String,
-}
-
-pub async fn update_symbol(args: UpdateSymbolArgs) -> Result<(), ActionError> {
-    let old_md = decode(&args.client, &args.mint_account)
+pub async fn update_symbol(
+    client: &RpcClient,
+    keypair: Keypair,
+    mint_account: &Pubkey,
+    new_symbol: &str,
+) -> AnyResult<Signature> {
+    let old_md = decode(&client, &mint_account)
         .await
-        .map_err(|e| ActionError::ActionFailed(args.mint_account.to_string(), e.to_string()))?;
+        .map_err(|e| ActionError::ActionFailed(mint_account.to_string(), e.to_string()))?;
     let data_with_old_symbol = old_md.data;
 
     let new_data = DataV2 {
         creators: data_with_old_symbol.creators,
         seller_fee_basis_points: data_with_old_symbol.seller_fee_basis_points,
         name: data_with_old_symbol.name,
-        symbol: args.new_symbol.to_owned(),
+        symbol: new_symbol.to_owned(),
         uri: data_with_old_symbol.uri,
         collection: old_md.collection,
         uses: old_md.uses,
     };
 
-    let mint_account = args.mint_account;
-    update_data(&args.client, &args.keypair, &mint_account, new_data)
+    let mint_account = mint_account;
+    let sig = update_data(&client, &keypair, &mint_account, new_data)
         .await
-        .map_err(|e| ActionError::ActionFailed(args.mint_account.to_string(), e.to_string()))?;
-    Ok(())
+        .map_err(|e| ActionError::ActionFailed(mint_account.to_string(), e.to_string()))?;
+    Ok(sig)
 }
